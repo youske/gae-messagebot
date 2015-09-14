65bbc1f731abddd2d3676c2324670c0bb8318580
Vagrant User <vagrant@vmachine>
2015-09-14 16:31:53 +0900

    chatwork task support

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import sys
 import urllib
+import time
+import datetime
+import dateutil.parser
 
 from urlparse import urlparse
 
@@ -27,10 +30,7 @@ jn2 = jinja2.Environment(
     autoescape=True)
 
 
-
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
-
+CHATWORK_API_TOKEN = '9faad66c13223a882c6e7f01fe22dace'
 
 @app.route('/')
 @login_required
@@ -49,7 +49,7 @@ def event():
 def message(room_id):
   Url="https://api.chatwork.com/v1/rooms/%d/messages" % room_id
   form_fields = {
-    "body": "body data not found"
+    "body": "no message body"
   }
 
   if request.method == 'POST':
@@ -62,13 +62,41 @@ def message(room_id):
   result = urlfetch.fetch(url=Url,
     payload=form_data,
     method=urlfetch.POST,
-    headers={'Content-Type': 'application/x-www-form-urlencoded', 'X-ChatWorkToken': ''})
+    headers={'Content-Type': 'application/x-www-form-urlencoded', 'X-ChatWorkToken': CHATWORK_API_TOKEN })
 
   return 'ok'
 
 
+
 @app.route('/task/<int:room_id>', methods=['GET','POST'])
+#@login_required
 def task(room_id):
+  Url="https://api.chatwork.com/v1/rooms/%s/tasks" % room_id
+  form_fields = {
+   'body': "no task title",
+   'to_ids': 1049263 
+  }
+
+  if request.method == 'GET':
+    form_fields['body'] = request.args.get("body").encode('utf-8')
+    dt = datetime.datetime.strptime( request.args.get('expire') )
+    form_fields['limit'] = int( time.mktime( dt.timetuple() ) )  
+    form_fields['to_ids'] = request.args.get('to_ids').encode('utf-8')
+
+  if request.method == 'POST':
+    if request.form.has_key('expire'): 
+      dt = dateutil.parser.parse( request.form['expire'] )
+      form_fields['limit'] = int( time.mktime( dt.timetuple() ) ) 
+    
+    form_fields['body'] = request.form['body'].encode('utf-8') if request.form.has_key('body') else null
+    form_fields['to_ids'] = request.form['to_ids'] if request.form.has_key('to_ids') else null
+
+  form_data = urllib.urlencode( form_fields )
+  urlfetch.fetch(url=Url,
+    payload=form_data,
+    method=urlfetch.POST,
+    headers={'Content-Type': 'application/x-www-form-urlencoded', 'X-ChatWorkToken': CHATWORK_API_TOKEN})
+
   return 'ok'
 
 
